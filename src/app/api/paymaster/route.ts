ce6f49064e3e4a82118de5d5075b26b96da9e694
Theesit Konkaew <theesit@zipmex.com>
2024-06-20 10:00:55 +0700

    debug

--- src/app/api/paymaster/route.ts
+++ src/app/api/paymaster/route.ts
@@ -2,8 +2,6 @@ import { paymasterClient } from "../config";
 import { willSponsor } from "../utils";
 
 export async function POST(r: Request) {
-  console.log("paymaster route");
-  console.log("request", r);
   const req = await r.json();
   const method = req.method;
 
@@ -14,8 +12,11 @@ export async function POST(r: Request) {
   const [userOp, entrypoint, chainId] = req.params;
 
   if (!willSponsor({ chainId: parseInt(chainId), entrypoint, userOp })) {
+    console.log("not a sponsorable operation");
     return Response.json({ error: "Not a sponsorable operation" });
   }
+
+  console.log("is a sponsorable operation");
 
   if (method === "pm_getPaymasterStubData") {
     const result = await paymasterClient.getPaymasterStubData({
